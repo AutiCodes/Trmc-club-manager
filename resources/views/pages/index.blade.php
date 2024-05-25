d3fe90a34c1118211a6d2767676d3811991361d9
KelvinCodes <31496522+KelvinCodesStuff@users.noreply.github.com>
2024-05-25 13:52:57 +0200

    Update

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -33,6 +33,28 @@
 			<!-- END NAVBAR -->
 
 
+			<!-- Modal -->
+			<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					...
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+				</div>
+			</div>
+			</div>
+
+
 			<!-- BEGIN FLIGHT SUBMISSION FORM --> 
 			<form class="col-lg-6 offset-lg-3 pt-4 pb-4">
 				<div class="justify-content-center">
@@ -89,7 +111,7 @@
 				<span id="captcha" style="color:red"></span> <!-- this will show captcha errors -->
 
 				<!-- SEND FORM BUTTON -->
-				<button type="button" class="btn btn-success font-weight-bold">Verzenden</button>
+				<button type="button" class="btn btn-success font-weight-bold" data-toggle="modal" data-target="#exampleModalCenter">Verzenden</button>
 			</form>
 			<!-- END FLIGHT SUBMISSION FORM -->
 		</main>
