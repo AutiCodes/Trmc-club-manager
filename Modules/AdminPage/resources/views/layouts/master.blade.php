5d112806adf2c11dc69d9457e22733d765055893
KelvinCodes <31496522+KelvinCodesStuff@users.noreply.github.com>
2024-05-25 13:45:01 +0200

    Update

--- Modules/AdminPage/resources/views/layouts/master.blade.php
+++ Modules/AdminPage/resources/views/layouts/master.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,6 +16,10 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <!-- Styles -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<!-- tab icon -->
+	<link rel="icon" href="{{ url('/media/images/TRMC_LOGO_PNG.ico') }}" type="image/x-icon">
     {{-- Vite CSS --}}
     {{-- {{ module_vite('build-adminpage', 'resources/assets/sass/app.scss') }} --}}
 </head>
@@ -26,4 +29,11 @@
 
     {{-- Vite JS --}}
     {{-- {{ module_vite('build-adminpage', 'resources/assets/js/app.js') }} --}}
+    <!-- JS --> 
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<!-- Google reCCHAPTA -->
+	<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </body>
