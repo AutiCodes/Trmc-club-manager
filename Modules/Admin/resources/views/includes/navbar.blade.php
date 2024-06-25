2da7ed39ef69b62a5720c0f37dc80d17f553e96c
KelvinCodes <31496522+KelvinCodesStuff@users.noreply.github.com>
2024-06-25 12:42:14 +0200

    Update navbar admin page

--- Modules/Admin/resources/views/includes/navbar.blade.php
+++ Modules/Admin/resources/views/includes/navbar.blade.php
@@ -1,5 +1,5 @@
 <!-- NAVBAR -->
-<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-dark">
   <a class="navbar-brand text-white" href="/">
     <img src="/media/images/TRMC_LOGO_PNG.ico" width="30" height="30" class="d-inline-block align-top" alt="">
     TRMC club manager
@@ -30,18 +30,20 @@
           <a class="dropdown-item" href="{{ route('members.create') }}">Lid/bestuur toevoegen</a>
         </div>
       </li>         
-      <!-- TODO: align right -->
-      <li class="nav-item dropdown">
+    </ul>
+
+    <ul class="navbar-nav ml-auto"> 
+      <li class="nav-item dropdown mr-3">
         <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Welkom {{ Auth::user()->name }}
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="{{ route('users.create') }}">Voeg een admin login toe</a>
+          <a class="dropdown-item" href="{{ route('users.create') }}">Voeg een admin toe</a>
           <a class="dropdown-item" href="{{ route('users.edit', auth()->user()->id) }}">Wijzig je profiel</a>
           <a class="dropdown-item" href="/authenticatie-uitloggen">Log jezelf uit</a>
         </div>
-      </li>                                      
-    </ul>
+      </li>  
+    </ul> 
   </div>
 </nav>
 <!-- END NAVBAR -->
