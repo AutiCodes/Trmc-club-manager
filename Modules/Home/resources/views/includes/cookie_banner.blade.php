00171f958db789eeee6812cf76a967689a23c2ef
KelvinCodes <31496522+KelvinCodesStuff@users.noreply.github.com>
2024-06-30 14:43:16 +0200

    Added home cookie banner + accept

---
+++ Modules/Home/resources/views/includes/cookie_banner.blade.php
@@ -0,0 +1,11 @@
+<div class="cookie_banner text-center" id="cookie_banner">
+      <p class="text-dark pt-2">  
+        Deze website gebruikt 
+        <img src="/media/images/icons/cookie.png" style="width: 25px"></img> 
+        (cookies).
+        Door deze website te gebruiken ga je hier akkoord mee.
+        <a href="#" class="btn btn-success" onclick="cookie_choice()">
+          Akkoord
+        </a>
+      </p>
+</div>
