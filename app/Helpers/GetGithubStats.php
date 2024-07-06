9566972241c0e4b7cb67248ab6d5bbb58d096972
KelvinCodes <31496522+KelvinCodesStuff@users.noreply.github.com>
2024-07-06 22:34:50 +0200

    Added github curl helper function

---
+++ app/Helpers/GetGithubStats.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Helpers;
+
+class GetGithubStats
+{
+    /**
+     * Curl the Github API
+     * 
+     * @param string $url the url to curl
+     * @return mixed
+     */
+    public static function get($url)
+    {
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 100);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 100);
+        curl_setopt($curl, CURLOPT_AUTOREFERER, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'Chrome/64.0.3282.186 Safari/537.36');
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: ' . env('GITHUB_ACCESS_TOKEN')));
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+
+        $result = json_decode(curl_exec($curl));
+        curl_close($curl);
+
+        return $result;
+    }
+}
