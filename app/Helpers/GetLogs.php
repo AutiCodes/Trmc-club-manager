6440e516ab1ca0ec940cf269cb4b0adfc60d2246
KelvinCodes <31496522+KelvinCodesStuff@users.noreply.github.com>
2024-07-06 22:45:09 +0200

    Refactored get logs to helper function

---
+++ app/Helpers/GetLogs.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Helpers;
+
+class GetLogs
+{
+    /**
+     * Gets the Laravel log file
+     * 
+     * @author KelvinCodes
+     * @return array logs
+     */
+    public static function laravel()
+    {
+        return array_reverse(file(storage_path('logs/laravel.log')));
+    }
+
+    /**
+     * Gets the Laravel log file
+     * 
+     * @author KelvinCodes
+     * @return array logs
+     */    
+    public static function userActivity()
+    {
+        return array_reverse(file(storage_path('logs/user_activity.log')));
+    }
+
+    /**
+     * Gets the Laravel log file
+     * 
+     * @author KelvinCodes
+     * @return array logs
+     */    
+    public static function memberActivity()
+    {
+        return array_reverse(file(storage_path('logs/member_activity.log')));
+    }
+
+    /**
+     * Gets the Laravel log file
+     * 
+     * @author KelvinCodes
+     * @return array logs
+     */    
+    public static function access()
+    {
+        return array_reverse(file(storage_path('logs/access.log')));
+    }
+}
