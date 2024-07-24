f603ff15e70bb83504ea7fffb50f068da08a5ef9
KelvinCodes <prive@kelvincodes.nl>
2024-07-24 11:12:51 +0200

    Added settings page, closes Add settings page #153

---
+++ Modules/Settings/resources/views/pages/index.blade.php
@@ -0,0 +1,16 @@
+@extends('admin::layouts.master')	
+
+@section('title', 'Instellingen')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        <div class="bg-dark bg-opacity-75 pt-2 pb-2 pl-2 pr-2 mb-2 mt-2 rounded ">
+          <h4 class="font-weight-bold text-white mb-0 ps-2">Algemeen</h4>
+          <p class="text-white ps-2">Work in progress</p>
+        </div>
+      </div>
+    </div>
+  </div>
+@stop
