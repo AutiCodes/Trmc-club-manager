5c28c0891f0d7326a176a6a237e196d16b4892a1
AutiCodes <prive@auticodes.nl>
2024-08-11 15:08:31 +0200

    Moved seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,10 +11,20 @@ use Illuminate\Support\Str;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
+     * Creates an default user to sign in to.
+     * 
+     * @author AutiCodes
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Default user',
+            'username' => 'Admin',
+            'password' => Hash::make('Admin'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        $this->call([DefaultUserSeeder::class]);
     }
 }
