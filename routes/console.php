59752030b7acae1863831e64c7a187695fecb0e3
AutiCodes <prive@auticodes.nl>
2024-08-12 15:55:31 +0200

    Temp fix empty form fill-ins Closes Fix empty form fill ins #165

--- routes/console.php
+++ routes/console.php
@@ -5,11 +5,17 @@ use Illuminate\Support\Facades\Artisan;
 use Modules\Fail2Ban\Models\Fail2Ban;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
+use Modules\Form\Models\Form;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
+/**
+ * Deletes banned users/IP's from Fail2Ban
+ * 
+ * @author AutiCodes
+ */
 Schedule::call(function () {
     $bans = Fail2Ban::all();
 
@@ -19,5 +25,25 @@ Schedule::call(function () {
             $ban->delete();
         } 
 
+    }
+})->everyMinute();
+
+/**
+ * Cleans up empty form fill ins
+ * TODO Temp fix, needs code refactoring 
+ * 
+ * @author AutiCodes
+ */
+Schedule::call(function () {
+    $flightSubmittions = Form::orderBy('id', 'desc')
+                            ->with('member')
+                            ->with('submittedModels')
+                            ->get();
+
+    foreach ($flightSubmittions as $flightSubmittion) {
+        if (count($flightSubmittion->submittedModels) == 0) {
+            // Delete flight
+            $flightSubmittion->delete();
+        } 
     }
 })->everyMinute();
