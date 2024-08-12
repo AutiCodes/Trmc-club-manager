e1abcdb08b5a95791ca6bd67195238c6a9a5a60a
AutiCodes <prive@auticodes.nl>
2024-08-12 11:17:22 +0200

    Added Fail2ban in admin menu

--- Modules/Admin/resources/views/includes/navbar.blade.php
+++ Modules/Admin/resources/views/includes/navbar.blade.php
@@ -8,11 +8,13 @@
       <path d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849m.894.448C7.111 2.02 7 2.569 7 3v4a.5.5 0 0 1-.276.447l-5.448 2.724a.5.5 0 0 0-.276.447v.792l5.418-.903a.5.5 0 0 1 .575.41l.5 3a.5.5 0 0 1-.14.437L6.708 15h2.586l-.647-.646a.5.5 0 0 1-.14-.436l.5-3a.5.5 0 0 1 .576-.411L15 11.41v-.792a.5.5 0 0 0-.276-.447L9.276 7.447A.5.5 0 0 1 9 7V3c0-.432-.11-.979-.322-1.401C8.458 1.159 8.213 1 8 1s-.458.158-.678.599"/>
     </svg>
   </button>
+
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link text-white" href="/">Home</a>
       </li>
+
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Vluchten
@@ -22,6 +24,7 @@
           <a class="dropdown-item" href="/downloadFlightsGov">Alle vluchten (laatste 365 dagen) voor de gemeente</a>
         </div>
       </li>
+
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Leden
@@ -31,6 +34,7 @@
           <a class="dropdown-item" href="{{ route('members.create') }}">Lid toevoegen</a>
         </div>
       </li>         
+
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Bestuur
@@ -49,6 +53,7 @@
           <a class="dropdown-item" href="{{ route('settings.index') }}">Instellingen</a>
           <a class="dropdown-item" href="{{ route('versions.index') }}">Versies</a>
           <a class="dropdown-item" href="{{ route('logs.index') }}">Logs</a>
+          <a class="dropdown-item" href="{{ route('fail2ban.index') }}">Fail2Ban</a>
         </div>
       </li>     
 
@@ -65,5 +70,6 @@
         </div>
       </li>  
     </ul> 
+
   </div>
 </nav>
