3b0a652b11e0552820bbe9ef32d2ca382d8e1901
KelvinCodes <31496522+KelvinCodesStuff@users.noreply.github.com>
2024-08-25 00:25:39 +0200

    Added settings

--- Modules/Settings/routes/web.php
+++ Modules/Settings/routes/web.php
@@ -14,6 +14,6 @@ use Modules\Settings\Http\Controllers\SettingsController;
 |
 */
 
-Route::group([], function () {
+Route::group(['middleware', 'auth'], function () {
     Route::resource('settings', SettingsController::class)->names('settings');
 });
