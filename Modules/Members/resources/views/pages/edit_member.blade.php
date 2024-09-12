8fb4f9d5d409964ebb4da35f6726fd6a0e56c4b6
KelvinCodes <prive@auticodes.nl>
2024-09-12 23:08:04 +0200

    Made edit member same as other UI

--- Modules/Members/resources/views/pages/edit_member.blade.php
+++ Modules/Members/resources/views/pages/edit_member.blade.php
@@ -8,9 +8,9 @@
     @csrf
     @method('PUT')
 
-    <div class="row">
+    <div class="row bg-dark rounded bg-opacity-75">
       <div class="col-sm">
-        <div class="bg-dark pt-2 pb-2 pl-2 pr-2 mb-2 mt-2 rounded">
+        <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
           <div class="form-group">
             <label for="name" class="text-white font-weight-bold">Volledige naam</label>
             <input type="text" class="form-control" id="name" name="name" aria-describedby="fullname" placeholder="Voornaam achternaam" required value="{{ $member->name }}">
@@ -30,12 +30,12 @@
             <label for="address"  class="text-white font-weight-bold">Adres</label>
             <input type="text" class="form-control" id="address" name="address" placeholder="Straatnaam nummer" required value="{{ $member->address }}">
           </div>
-          
+
           <div class="form-group">
             <label for="postcode"  class="text-white font-weight-bold">Postcode</label>
             <input type="text" class="form-control" id="postcode" name="postcode" placeholder="1234AH (zonder spatie!)" required value="{{ $member->postcode }}">
           </div>
-          
+
           <div class="form-group">
             <label for="city"  class="text-white font-weight-bold">Woonplaats</label>
             <input type="text" class="form-control" id="city" name="city" placeholder="Woonplaats" required value="{{ $member->city }}">
@@ -49,32 +49,32 @@
             <label for="phone"  class="text-white font-weight-bold">Telefoon</label>
             <input type="text" class="form-control" id="phone" name="phone" placeholder="0612343455 (nummer zonder streepje!)" required value="{{ $member->phone }}">
           </div>
-          
+
           <div class="form-group">
             <label for="email" class="text-white font-weight-bold">E-mail</label>
             <input type="email" class="form-control" id="email" name="email" placeholder="mail@provider.nl" required value="{{ $member->email }}">
-          </div>   
+          </div>
         </div>
       </div>
     </div>
 
-    <div class="row">
+    <div class="row bg-dark rounded bg-opacity-75 mt-2">
       <div class="col-sm">
-        <div class="bg-dark pt-2 pb-2 pl-2 pr-2 mb-2 mt-2 rounded">
+        <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
           <div class="form-group">
             <label for="rdw_number"  class="text-white font-weight-bold">RDW nummer</label>
             <input type="text" class="form-control" id="rdw_number" name="rdw_number" placeholder="A34hjhdwqjkwqa" value="{{ $member->rdw_number }}">>
-          </div>    
+          </div>
 
           <div class="form-group">
             <label for="knvvl"  class="text-white font-weight-bold">KNVvl nummer</label>
             <input type="text" class="form-control" id="knvvl" name="knvvl" placeholder="1234567" value="{{ $member->KNVvl }}">
-          </div>   
+          </div>
         </div>
       </div>
 
       <div class="col-sm">
-        <div class="bg-dark pt-2 pb-2 pl-2 pr-2 mb-2 mt-2 rounded">
+        <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
           <div class="form-group">
             <label for="club_status" class="text-white font-weight-bold">Club status</label>
             <select class="form-control" id="club_status" name="club_status" required>
@@ -97,7 +97,7 @@
                 <option value=3 selected>Bestuur</option>
               @else
                 <option value=3>Bestuur</option>
-              @endif          
+              @endif
               @if ($member->club_status == \Modules\Members\Enums\ClubStatus::DONOR->value)
                 <option value=5 selected>Donateur</option>
               @else
@@ -112,7 +112,7 @@
                 <option value=8 selected>Nieuwe aanmelding</option>
               @else 
                 <option value=8>Nieuwe aanmelding</option>
-              @endif              
+              @endif
             </select>
           </div> 
 
@@ -135,7 +135,7 @@
       </div>
 
       <div class="col-sm">
-        <div class="bg-dark pt-2 pb-2 pl-2 pr-2 mb-2 mt-2 rounded">
+        <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
           <p class="font-weight-bold text-white mb-0">Brevetten</p>
           <div class="form-check">
             @if ($member->has_plane_brevet == 1)
@@ -183,37 +183,38 @@
           </div>
         </div>
       </div>
+    </div>
 
-      <div class="row">
-      <div class="col-sm">
-        <div class="bg-dark pt-2 pb-2 ps-2 pe-2 mb-2 mt-2 rounded w-25">
-          <h4 class="font-weight-bold text-white mb-0">Drone certificaten</h4>
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" value=1 id="droneA1Checkbox" name="droneA1Checkbox" @if ($member->has_drone_a1 == 1) checked @endif>
-            <label class="form-check-label text-white" for="droneA1Checkbox">
-              A1
-            </label>
-          </div>
+      <div class="row bg-dark rounded bg-opacity-75 mt-2">
+        <div class="col-sm">
+          <div class="pt-2 pb-2 ps-2 pe-2 mb-2 mt-2 w-25">
+            <h4 class="font-weight-bold text-white mb-0">Drone certificaten</h4>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value=1 id="droneA1Checkbox" name="droneA1Checkbox" @if ($member->has_drone_a1 == 1) checked @endif>
+              <label class="form-check-label text-white" for="droneA1Checkbox">
+                A1
+              </label>
+            </div>
 
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" value=1 id="droneA2Checkbox" name="droneA2Checkbox" @if ($member->has_drone_a2 == 1) checked @endif>
-            <label class="form-check-label text-white" for="droneA2Checkbox">
-              A2
-            </label>
-          </div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value=1 id="droneA2Checkbox" name="droneA2Checkbox" @if ($member->has_drone_a2 == 1) checked @endif>
+              <label class="form-check-label text-white" for="droneA2Checkbox">
+                A2
+              </label>
+            </div>
 
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" value=1 id="droneA3Checkbox" name="droneA3Checkbox" @if ($member->has_drone_a3 == 1) checked @endif>
-            <label class="form-check-label text-white" for="droneA3Checkbox">
-              A3
-            </label>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value=1 id="droneA3Checkbox" name="droneA3Checkbox" @if ($member->has_drone_a3 == 1) checked @endif>
+              <label class="form-check-label text-white" for="droneA3Checkbox">
+                A3
+              </label>
+            </div>
           </div>
         </div>
+        <button type="submit" class="btn btn-success">Update</button>
+
       </div>
     </div>
-
-    </div>
-    <button type="submit" class="btn btn-primary">Update</button>
   </form>
 </div>
 @endsection
