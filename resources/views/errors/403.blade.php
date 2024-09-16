17ab2b1101649090b9ec7dd99a1d34b2b22a2341
KelvinCodes <prive@auticodes.nl>
2024-09-16 11:33:26 +0200

    update

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,9 @@
+@extends('home::layouts.master')
+
+@section('title', 'TRMC club manager')
+
+@section('content')
+  <div class="container mt-4">
+    
+  </div>
+@stop
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('home::layouts.master')
+
+@section('title', 'TRMC club manager')
+
+@section('content')
+  <div class="container mt-4 text-center">
+    <h1 class="text-white">Oeps!<h1>
+    
+  </div>
+@stop
